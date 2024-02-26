7e7b86dd23c6f15231ed3e4814a6838b466c2361
Ben <144111469+bengilbertcodes@users.noreply.github.com>
2024-02-26 16:02:55 +0000

    Third commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -33,3 +33,10 @@ const sayHello = () => console.log('hello');
 sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+	`<p>
+	 This is a multiline string!
+	</p>`
+)
+console.log(returnMultipleLines())
